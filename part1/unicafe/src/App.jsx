1987dfd7589430fca84aab027c899b1cd8c3de10
tejeshreddy <tejeshreddy111@gmail.com>
2023-10-12 15:09:07 -0500

    update: unicafe complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const StatisticLine = (props) => {
+  const { text, value } = props;
+  return (
+    <tr>
+      <td>
+        {text} {value}
+      </td>
+    </tr>
+  );
+};
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+
+  return good + neutral + bad === 0 ? (
+    <div>No feedback given</div>
+  ) : (
+    <table>
+      <tbody>
+        <tr>
+          <td>statistics</td>
+        </tr>
+
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good + bad + neutral} />
+        <StatisticLine
+          text="average"
+          value={(good * 1 + neutral * 0 + bad * -1) / (good + bad + neutral)}
+        />
+        <StatisticLine text="positive" value={good / (good + bad + neutral)} />
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- part1/unicafe/src/main.jsx
+++ part1/unicafe/src/main.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client.js';
-import App from './App.jsx';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
