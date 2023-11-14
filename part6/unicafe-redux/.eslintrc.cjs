7dae406316ac477a442233b27b709fc81dcca20c
tejeshreddy <tejeshreddy111@gmail.com>
2023-11-13 18:10:54 -0600

    6.1 - 6.2

--- part6/unicafe-redux/.eslintrc.cjs
+++ part6/unicafe-redux/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, "jest/globals": true },
+  env: { browser: true, es2020: true, 'jest/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -11,10 +11,11 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
+  semi: 'off',
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
