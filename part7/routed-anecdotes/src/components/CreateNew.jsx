6e1e8b7060830c4b60a1b6c82bcc3a1e04c6037a
tejeshreddy <tejeshreddy111@gmail.com>
2023-11-21 16:00:24 -0600

    7.4 - 7.6

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import { useField } from '../hooks';
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate('/anecdotes');
   };
+
+  const handleReset = (e) => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
+  const filterProps = (obj, keysToExclude) =>
+    Object.fromEntries(
+      Object.entries(obj).filter(([key]) => !keysToExclude.includes(key))
+    );
 
   return (
     <div>
@@ -25,29 +37,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...filterProps(content, ['reset'])} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...filterProps(author, ['reset'])} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...filterProps(info, ['reset'])} />
         </div>
         <button>create</button>
+        <button type="button" onClick={() => handleReset()}>
+          reset
+        </button>
       </form>
     </div>
   );
